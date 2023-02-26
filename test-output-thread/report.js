e902fe73400ba5a16b802e6671e275772b98052b
SOURAV <sourav2864@gmail.com>
2023-02-26 16:50:18 +0530

    all project files

---
+++ test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "79d5ed6c-9df5-4a9d-bf93-c5830c19e16b",
+    "feature": "feature to test functionality of -\u003e Simple Form Demo | Drag \u0026 Drop Sliders | Input Form Submit [Story ID - xxxxx]",
+    "scenario": "Simple Form Demo [Scenario ID - xxxxx]",
+    "start": 1677410063036,
+    "group": 1,
+    "content": "",
+    "tags": "@123,@101,",
+    "end": 1677410080847,
+    "className": "passed"
+  },
+  {
+    "id": "9cf43ccd-0044-4e21-a3e8-c4446d9c03c5",
+    "feature": "feature to test functionality of -\u003e Simple Form Demo | Drag \u0026 Drop Sliders | Input Form Submit [Story ID - xxxxx]",
+    "scenario": "“Drag \u0026 Drop Sliders [Scenario ID - xxxxx]",
+    "start": 1677410090348,
+    "group": 1,
+    "content": "",
+    "tags": "@123,@103,",
+    "end": 1677410104894,
+    "className": "passed"
+  },
+  {
+    "id": "386ba046-04e7-4afe-b205-e274a7bcac40",
+    "feature": "feature to test functionality of -\u003e Simple Form Demo | Drag \u0026 Drop Sliders | Input Form Submit [Story ID - xxxxx]",
+    "scenario": "“Drag \u0026 Drop Sliders [Scenario ID - xxxxx]",
+    "start": 1677410080879,
+    "group": 1,
+    "content": "",
+    "tags": "@123,@102,",
+    "end": 1677410090348,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
